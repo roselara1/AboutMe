200edefa7b26a4d2794ba60275c39bd4fbfde3a0
Rosie Lara <roselara1@gmail.com>
2018-01-13 23:04:33 -0800

    points

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
-
+//keep track of user points
+var points = 0
 //greet and get user's name
 
 alert('Well hello there! This page will tell you a bit about me, but before we get there, let\'s learn something about you first. Perhaps, your name?');
@@ -21,6 +22,7 @@ if (answer1 === 'No' || answer1 ==='no'){
 }
 else {
   alert('Correct! Sweet treats are my jam! Get it?');
+  points++;
 }
 
 //ask question 2
@@ -32,6 +34,7 @@ if (answer2 === 'yes' || answer2 === 'Yes'){
   alert('Aww I wish! I have mostly traveled around Asia, but Europe will happen one day.');}
 else {
   alert('You\'re good at this! Hmm, do you know me?...' );
+  points++;
 }
 
 //ask question 3
@@ -44,6 +47,7 @@ if (answer3 === 'Yes' || answer3 ==='yes'){
 }
 else {
   alert('Correct, I was born in a beautiful country named Dominican Republic.');
+  points++;
 }
 
 //question 4
@@ -53,6 +57,7 @@ var answer4 = prompt('Can you name my hometown? Here\'s a hint, it\'s known as t
 console.log('The user answered question 1' + answer4);
 if (answer4 === 'nyc' || answer4 ==='new york'|| answer4 ==='new york city'|| answer4 ==='manhattan'|| answer4 ==='the center of the universe'){
   alert('Yes! I moved to Manhattan when I was 3 years old love it very much!');
+  points++;
 }
 else {
   alert( 'That was a great guess, but it was actually New York City. I moved there when I was 3 years old and it is still my favorite city in the world.');
@@ -64,6 +69,7 @@ var answer5 = prompt('Have I worked overseas?').toLowerCase();
 console.log('The user answered question 1' + answer3);
 if (answer5 === 'Yes' || answer5 ==='yes'){
   alert('Correct! I worked as an English teacher for several years in South Korea. I taught everything from business English to engineers, to basic English to preschoolers.');
+  points++;
 }
 else {
   alert('Actually, I worked as an English teacher for several years in South Korea. I taught everything from business English to engineers, to basic English to preschoolers.');
@@ -72,24 +78,29 @@ alert('Question 6');
 
 //ask question 6 use a for loop
 var answer6 = 2;
-var answer6 = prompt('How many countries did I travel to in 2017', 'Enter a number between 1 - 10').toLowerCase();
+var guess;
+var guess = prompt('How many countries did I travel to in 2017', 'Enter a number between 1 - 10').toLowerCase();
 console.log('The user answered question 1' + answer6);
 for (i = 6; i < 6; i++);{
   console.log(i);
-  if (answer6 > 2){
+  if (answer6 === 2);{
+    alert('Correct! I visited a total of 2 countries.');
+    console.log(answer6);
+    points++;
+  }
+  else {
+    if (answer6 > 2);{
     alert ('Too high a number. Try again.');
     console.log(answer6);
-  }
-  if (answer6 < 2){
+    }
+    else (answer6 < 2);{
     alert ('Too low a number. Try again');
     console.log(answer6);
   }
-  if (answer6 === 2) {
-    alert('Correct! I visited a total of 2 countries.');
-    console.log(answer6);
   }
-  //end
 }
+  //end
+
 
 alert('Question 7');
 
@@ -98,6 +109,7 @@ var answer7 = prompt('Now that you know I went to 2 countries, can you guess one
 console.log('The user answered question 1' + answer5);
 if (answer7 === 'Thailand' || answer7 ==='thailand'){
   alert('Yup! I stayed in Thailand for 12 wonderful days.');
+  points++;
 }
 while (guess < 6){
   alert ('Enter the name of the country');
@@ -120,7 +132,7 @@ while (guess < 6){
   if (answer7 !== 'Thailand' || 'thailand'); {
     alert('No more tries left. The correct answer was Thailand. You should visit sometime.');
   }
-  //break to end the loop
-  break;
 }
+
+alert ( 'You answered ' + points + 'out of 7 questions correctly!');
 
