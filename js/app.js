b4f5b506a0b66b843fb9bb77d1f5e3e695cea33c
Rosie <15274209+roselara1@users.noreply.github.com>
2018-01-20 08:57:56 -0800

    Lab 3 fixed

--- js/app.js
+++ js/app.js
@@ -1,58 +1,129 @@
 'use strict';
+//keep track of user points
+var points = 0;
+//greet and get user's name
 
-alert('Hello there! Today we will play a little yes or no game featuring me!');
+alert('Well hello there! This page will tell you a bit about me, but before we get there, let\'s learn something about you first. Perhaps, your name?');
 
+var user = prompt('What\'s your name?');
+console.log('The user\'s name is ' + user);
 
-alert('This is a yes or no question, so please answer with yes or no.' );
+//ask question 1
+
+alert ('Hi there, ' + user + '. I\'m going to ask you a few questions to teach you some things about me.');
+while (!user){
+  user = prompt('What is your name?,', 'type your name here');
+}
+alert('This is a yes or no question, so please answer with yes or no ' + user +'.' );
 var answer1 = prompt('Do I like baking?').toLowerCase();
 console.log('The user answered question 1' + answer1);
 if (answer1 === 'No' || answer1 ==='no'){
-  alert('Um, what?! Of course I do!');
+  alert('Of course I do!');
 }
 else {
   alert('Correct! Sweet treats are my jam! Get it?');
+  points++;
 }
+alert('You have ' + points + ' out of 7!');
 
+//ask question 2
 alert('Question 2');
 
 var answer2 = prompt('Have I been to Europe?').toLowerCase();
 console.log('The user answered question 1' + answer2);
 if (answer2 === 'yes' || answer2 === 'Yes'){
-  alert('Aww I wish! I have mostly traveled around Asia, but Europe will happen one day.');    }
+  alert('Aww I wish! I have mostly traveled around Asia, but Europe will happen one day.');}
 else {
-  alert('You\'re good at this! Wait, do you know me? O.o' );
+  alert('You\'re good at this! Hmm, do you know me?...' );
+  points++;
 }
-
+alert('You have ' + points + ' out of 7!');
+//ask question 3
 alert('Question 3');
 
-var answer3 = prompt('Do I watch sports?').toLowerCase();
+var answer3 = prompt('Was I born in the United States?').toLowerCase();
 console.log('The user answered question 1' + answer3);
 if (answer3 === 'Yes' || answer3 ==='yes'){
-  alert('Nope! Nice try though.');
+  alert('Nope! I was actually born in a beautiful country, surrounded by beaches, named Dominican Republic.');
 }
 else {
-  alert('Yup! I do not care for sports at all.');
+  alert('Correct, I was born in a beautiful country named Dominican Republic.');
+  points++;
 }
-
+alert('You have ' + points + ' out of 7!');
+//question 4
 alert('Question 4');
 
-var answer4 = prompt('Do I live in Washington state?').toLowerCase();
+var answer4 = prompt('Can you name my hometown? Here\'s a hint, it\'s known as the "Big Apple".').toLowerCase();
 console.log('The user answered question 1' + answer4);
-if (answer4 === 'Yes' || answer4 ==='yes'){
-  alert('Yes, I do! For now...');
+if (answer4 === 'nyc' || answer4 ==='new york'|| answer4 ==='new york city'|| answer4 ==='manhattan'|| answer4 ==='the center of the universe'){
+  alert('Yes! I moved to Manhattan when I was 3 years old love it very much!');
+  points++;
 }
 else {
-  alert( 'Sorry, I actually DO live in Washington state.');
+  alert( 'That was a great guess, but it was actually New York City. I moved there when I was 3 years old and it is still my favorite city in the world.');
 }
-
+alert('You have ' + points + ' out of 7!');
+//ask question 5
 alert('Question 5');
 
-var answer5 = prompt('Is my favorite color blue?').toLowerCase();
-console.log('The user answered question 1' + answer5);
+var answer5 = prompt('Have I worked overseas?').toLowerCase();
+console.log('The user answered question 1' + answer3);
 if (answer5 === 'Yes' || answer5 ==='yes'){
-  alert('Blue is a nice color, but not my favorite. My favorite color is purple');        }
-else {
-  alert('Correct! My favorite color is purple.');
-
-  alert('Thank you! I hope to learn about you someday.');
+  alert('Correct! I worked as an English teacher for several years in South Korea. I taught everything from business English to engineers, to basic English to preschoolers.');
+  points++;
 }
+else {
+  alert('Actually, I worked as an English teacher for several years in South Korea. I taught everything from business English to engineers, to basic English to preschoolers.');
+}
+alert('You have ' + points + ' out of 7!');
+alert('Question 6');
+
+//ask question 6 use a for loop
+var answer6 = 2;
+
+for (var i = 6; i >= 0; i--){
+  var guess = prompt('How many countries did I travel to in 2017', 'Enter a number between 1 - 10').toLowerCase();
+  console.log('The user answered question 1' + guess);
+
+
+  if (guess > answer6) {
+    alert ('Too high a number. Try again.');
+    console.log(guess);
+  }
+  if (guess < answer6) {
+    alert ('Too low a number. Try again');
+    console.log(guess);
+  }
+  if (guess === '2' || guess === 'two') {
+    alert('Correct! I visited a total of 2 countries.');
+    console.log(guess);
+    points++;
+    break;
+  }
+}
+
+//end;
+alert('You have ' + points + ' out of 7!');
+
+alert('Question 7');
+
+//ask question 7 use a for loop
+var answer7 = 'Thailand';
+
+for (var i = 6; i >= 0; i--){
+  var guess7 = prompt('Now that you know I went to 2 countries, can you guess one of them? It starts with the letter "T", take a guess.').toLowerCase();
+  console.log('The user answered question 7' + guess7);
+
+
+  if (guess7 === 'Thailand' || guess7 ==='thailand'){
+    alert('Yup! I stayed in Thailand for 12 wonderful days.');
+    points++;
+    break;
+  }
+  if (answer7 !== 'Thailand' || 'thailand');{
+    alert ('No worries, you still have ' + i + '  more tries.');
+  }
+}
+
+alert ( 'You answered ' + points + ' out of 7 questions correctly!');
